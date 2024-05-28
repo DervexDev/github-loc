962c1aad4e7ccf7508eb6355f2c27884bae69b9c
Dervex <dervexdev@gmail.com>
2024-05-28 13:17:49 +0200

    Add request salt and and Ghloc-Authorization

--- src/stat/loader.ts
+++ src/stat/loader.ts
@@ -7,6 +7,15 @@ function makeKey(org: string, repo: string, branch: string) {
   return org + '/' + repo + '/' + branch
 }
 
+async function sha1(str: string) {
+  const encoder = new TextEncoder()
+  const hash = await crypto.subtle.digest('SHA-1', encoder.encode(str))
+
+  return Array.from(new Uint8Array(hash))
+    .map((v) => v.toString(16).padStart(2, '0'))
+    .join('')
+}
+
 export function loadLoc(org: string, repo: string, branch: string): Promise<number> {
   return new Promise((resolve) => {
     const key = makeKey(org, repo, branch)
@@ -24,15 +33,21 @@ export async function fetchLoc(
   repo: string,
   branch: string,
 ): Promise<[number, LocData]> {
+  let url = `https://ghloc-api.vercel.app/${org}/${repo}/${branch}`
+
   const accessToken = await chrome.storage.sync.get('accessToken')
-  const headers = new Headers()
+  const headers = new Headers({
+    'Ghloc-Authorization': import.meta.env.VITE_AUTH_TOKEN,
+  })
 
   if (typeof accessToken.accessToken === 'string' && accessToken.accessToken.length > 0) {
     headers.append('Authorization', `Bearer ${accessToken.accessToken}`)
+
+    url += '?salt=' + (await sha1(accessToken.accessToken))
   }
 
-  let data: LocData = await fetch(`https://ghloc-api.vercel.app/${org}/${repo}/${branch}`, {
-    headers: headers,
+  let data: LocData = await fetch(url, {
+    headers,
   })
     .then((res) => res.json())
     .then((data) => {
