c2f2a2df428d1df6256ad0e3d8e27d915d2eb585
Dervex <dervexdev@gmail.com>
2024-06-28 13:54:10 +0200

    Add CI and Release workflows, add release script

--- src/zip.js
+++ src/zip.js
@@ -1,10 +1,11 @@
-import gulp from 'gulp'
-import zip from 'gulp-zip'
-import { createRequire } from 'module'
+import gulp from "gulp"
+import zip from "gulp-zip"
+import { createRequire } from "module"
+
 const require = createRequire(import.meta.url)
-const manifest = require('../build/manifest.json')
+const manifest = require("../build/manifest.json")
 
 gulp
-  .src('build/**')
-  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
-  .pipe(gulp.dest('package'))
+  .src("build/**")
+  .pipe(zip(manifest.name.replaceAll(" ", "-").toLowerCase() + ".zip"))
+  .pipe(gulp.dest("./"))
